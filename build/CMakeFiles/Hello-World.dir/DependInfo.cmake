b8e418b57b9f71f658d403db2ae5c8494bd1f789
aj.given <ajgiven@new-test-ubunutu.myguest.virtualbox.org>
2023-03-06 12:45:05 -0800

    Initial Commit

---
+++ build/CMakeFiles/Hello-World.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ajgiven/Documents/learning-files/cmake_hello_world/Cmake-hello-world/source-files/hello_world.cxx" "CMakeFiles/Hello-World.dir/hello_world.cxx.o" "gcc" "CMakeFiles/Hello-World.dir/hello_world.cxx.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
